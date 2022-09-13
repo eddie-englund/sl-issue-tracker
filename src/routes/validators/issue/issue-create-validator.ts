8e6fc805b4f97f118541c01007a949ed3fa535f4
Eddie Englund <properllerbucher@gmail.com>
2022-09-13 21:39:24 +0200

    fix post

--- src/routes/validators/issue/issue-create-validator.ts
+++ src/routes/validators/issue/issue-create-validator.ts
@@ -1,3 +1,4 @@
+import { create } from 'domain';
 import { NextFunction, Request, Response } from 'express';
 import { z } from 'zod';
 
@@ -22,8 +23,14 @@ const issueCreateSchema = z.object({
     .trim()
     .min(1)
     .max(512),
-  created: z.date()
+  created: z.string()
 })
+
+const createdSchema = z.preprocess((arg) => {
+  if (typeof arg == "string" || arg instanceof Date) return new Date(arg);
+}, z.date());
+
+export type CreatedSchema = z.infer<typeof createdSchema>;
 
 export type IssueCreateSchema = z.infer<typeof issueCreateSchema>
 
@@ -31,7 +38,10 @@ export const issueCreateValidator = async (req: Request,
                                           res: Response,
                                           next: NextFunction) => {
   const parse = await issueCreateSchema.safeParseAsync(req.body)
+  const parseDate = await createdSchema.safeParseAsync(req.body.created)
   if (!parse.success) return res.status(400).send(parse.error)
+  if (!parseDate.success) return res.status(400).send(parseDate.error)
   req.body = parse.data
+  req.body.created = parseDate.data
   next()
 }
